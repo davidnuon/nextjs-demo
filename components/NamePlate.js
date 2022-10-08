b8ef6a5d509e756aeea658f876ef4f35fddc8c2e
David Nuon <davidnuongm@gmail.com>
2022-10-08 11:45:12 -0700

    Add counter

--- components/NamePlate.js
+++ components/NamePlate.js
@@ -1,3 +1,18 @@
-export default function NamePlate( { name }) {
-    return (<h1>{name}</h1>)
+import { useState } from 'react';
+
+export default function NamePlate({ name }) {
+    const [count, setCount] = useState(0)
+
+    return (<>
+        <h1>{name}: {count}</h1>
+        <button onClick={
+            () => {
+                setCount(count + 1)
+            }
+        }>Plus 1</button>
+        <button onClick={() => {
+            setCount(count - 1)
+        }
+        }>Minus 1</button>
+    </>)
 }
